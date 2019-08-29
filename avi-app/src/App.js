4a5459d22b8844a8f524e7cc080993dae8e08c9f
James Kim <daeho@Daeho.local>
2019-08-29 13:17:54 -0400

    cronos pro font

--- avi-app/src/App.js
+++ avi-app/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Axios from 'axios';
-import './App.css';
+import React from 'react'
+import Axios from 'axios'
+import './App.css'
 import Search from './components/search'
 import FoodList from './components/FoodList'
-
+import './components/FoodList.css'
 
 class App extends React.Component {
   constructor(props) {
@@ -17,69 +17,67 @@ class App extends React.Component {
     }
   }
   componentDidMount() {
-    this.getData();
+    this.getData()
     this.setState({ isLoaded: true })
   }
   getData = async () => {
-
     try {
-      const KEY = process.env.REACT_APP_TOKEN;
-      const ID = process.env.REACT_ID_TOKEN;
+      const KEY = process.env.REACT_APP_TOKEN
+      const ID = process.env.REACT_ID_TOKEN
 
-      const apiUrl = `https://api.edamam.com/search?q=${this.state.homePage}&app_id=${ID}&app_key=${KEY}&from=0&to=12`;
-      const apiResponse = await Axios.get(apiUrl);
+      const apiUrl = `https://api.edamam.com/search?q=${this.state.homePage}&app_id=${ID}&app_key=${KEY}&from=0&to=12`
+      const apiResponse = await Axios.get(apiUrl)
       const searchResult = apiResponse.data
       if (apiResponse.status === 200) {
         this.setState(prevState => ({
           recipes: searchResult.hits
-        })
-        )
+        }))
       }
     } catch (error) {
       console.log(error)
     }
-
   }
   AddInput = async () => {
     window.scrollTo(0, 300)
     try {
-      const KEY = process.env.REACT_APP_TOKEN;
-      const ID = process.env.REACT_ID_TOKEN;
-      const apiUrl = `https://api.edamam.com/search?q=${this.state.input}&app_id=${ID}&app_key=${KEY}&from=0&to=12`;
-      const apiResponse = await Axios.get(apiUrl);
+      const KEY = process.env.REACT_APP_TOKEN
+      const ID = process.env.REACT_ID_TOKEN
+      const apiUrl = `https://api.edamam.com/search?q=${this.state.input}&app_id=${ID}&app_key=${KEY}&from=0&to=12`
+      const apiResponse = await Axios.get(apiUrl)
       const newSearchResult = apiResponse.data
       if (apiResponse.status === 200) {
         this.setState(prevState => ({
           recipes: newSearchResult.hits
-        })
-        )
+        }))
       }
     } catch (error) {
       console.log(error)
     }
-
   }
-  HandleChange = (event) => {
-
+  HandleChange = event => {
     this.setState({
-      input: event.target.value,
-
+      input: event.target.value
     })
   }
-render() {
+  render() {
     return (
-
-
       <div className="App">
         <div className="banner">
-         <Search updateSearch={this.AddInput} handleChange={this.HandleChange} />
+          <Search
+            updateSearch={this.AddInput}
+            handleChange={this.HandleChange}
+          />
         </div>
-         <FoodList handleClick={this.HandleClick} recipes={this.state.recipes} recipe={this.props.recipes} state={this.state}/>
+        <div className='recipe-list-container'>
+          <FoodList
+            recipes={this.state.recipes}
+            recipe={this.props.recipes}
+            state={this.state}
+          />
+        </div>
       </div>
     )
   }
 }
 
-export default App;
-
-
+export default App
