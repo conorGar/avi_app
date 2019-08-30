a45d839e58b343f12e9713f72a240caa4e4442c9
Conor Garity <conorgarity@Conors-MBP.fios-router.home>
2019-08-30 06:52:27 -0400

    Imported Header onto recipePage

--- avi-app/src/components/RecipePage/index.js
+++ avi-app/src/components/RecipePage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import RecipeTop from './RecipeTop/index'
 import RecipeBottom from './RecipeBottom/index'
+import Header from '../Header/index'
 
 function RecipePage(){
     return (
         <div>
+            <Header />
             <RecipeTop />
             <RecipeBottom />
         </div>
