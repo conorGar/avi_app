32a3586dc3661c4d6531a72b2a18ebbc3f58d152
Chris <cjlee1191@gmail.com>
2019-09-03 11:50:16 -0400

    filter added

--- avi-app/src/components/Search/search.js
+++ avi-app/src/components/Search/search.js
@@ -43,21 +43,12 @@ class Search extends React.Component {
             <input className='search-bar-text'placeholder='Search Recipes' type="text" onChange={this.handleChange}/>
             <input className="search-bar-button"  value='Search' type="submit"></input>
             </form>
+            <form className="filter" onSubmit={this.handleSubmit}>
+                <div  className="drop-down" onClick={this.handleFilterPopup} placeholder="filter" >
+                See only the recipes right for you
+            </div>
+           </form>
 
-            <form className="filter" onSubmit={this.handleSubmit}> 
-                 <div  className="drop-down" onClick={this.handleFilterPopup} placeholder="filter" >
-                 See only the recipes right for you
-                 {/* <option value="">See only the recipes right for you</option>
-                  <option className="drop-down-option"value="vegan">Vegan</option>
-                  <option className="drop-down-option" value="vegetarian">Vegetarian</option>
-                  <option className="drop-down-option" value="keto">Keto</option>
-                  <option className="drop-down-option" value="gluten-free">Gluten Free</option>
-                  <option className="drop-down-option" value="paleo">Paleo</option>
-                  <option className="drop-down-option" value="low-sodium">Low Sodium</option>
-                  <option className="drop-down-option" value="dairy-free">Dairy Free</option> */}
-
-             </div>
-            </form>
         </div>
         <div className="inspire-box">
              <p className="inspire">Not sure what you are in the mood for? <button className="inspire-button">Inspire</button></p>
